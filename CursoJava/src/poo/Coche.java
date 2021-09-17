7f14f46dbe921a177f47589f787211cb6c01a235
Jose Perez <j.rperez@icloud.com>
2021-09-17 20:55:44 -0300

    Metodos constructores

--- CursoJava/src/poo/Coche.java
+++ CursoJava/src/poo/Coche.java
@@ -1,62 +1,61 @@
 package poo;
 
 public class Coche {
-	
-	private int ruedas; //Encapsulamos cada variable como private
+
+	private int ruedas; // Encapsulamos cada variable como private
 	private int largo;
 	private int ancho;
 	private int motor;
-	private int peso;
-	
+	@SuppressWarnings("unused")
+	private int peso_plataforma;
+	private String color;
+	private int peso_total;
+	private boolean asientos_cuero, climatizador;
+
 	public Coche() {
-		
+
 		ruedas = 4;
 		largo = 2000;
 		ancho = 300;
 		motor = 1600;
-		peso = 500;
+		peso_plataforma = 500;
+
+	}
+
+	public String dime_datos_generales() { // Getters
+		return "La plataforma del veihuculo tiente " + ruedas + " ruedas " + ". mide " + largo / 1000
+				+ " metros con un ancho de " + ancho + " cm y un peso de plataforma de " + peso_plataforma + " kg";
+	}
+
+	public void establece_color(String color_choche) { // Setters
+
+		color = color_choche;
+
+		// color = ("Azul");
+
+	}
+
+	public String dime_color() { // GETTERS
+		return "El color del coche es : " + color;
+
+	}
+	
+	public void configura_asientos(String asientos_cuero) { //SETTERS
+		
+		if(asientos_cuero == "si") {
+			this.asientos_cuero = true;
+		}else {
+			this.asientos_cuero = false;
+		}
+	}
+	public String dime_asientos() {
+		if(asientos_cuero == true) {
+			return "El coche tiene asientos de cuero";
+		}else {
+			return "El coche no tiene asientos de cuero";
+			
+		}
 		
 	}
 
-	public int getRuedas() {
-		return ruedas;
-	}
-
-	public void setRuedas(int ruedas) {
-		this.ruedas = ruedas;
-	}
-
-	public int getLargo() {
-		return largo;
-	}
-
-	public void setLargo(int largo) {
-		this.largo = largo;
-	}
-
-	public int getAncho() {
-		return ancho;
-	}
-
-	public void setAncho(int ancho) {
-		this.ancho = ancho;
-	}
-
-	public int getMotor() {
-		return motor;
-	}
-
-	public void setMotor(int motor) {
-		this.motor = motor;
-	}
-
-	public int getPeso() {
-		return peso;
-	}
-
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
-	
-	
 }
--- CursoJava/src/poo/Uso_Choche.java
+++ CursoJava/src/poo/Uso_Choche.java
@@ -5,9 +5,11 @@ public class Uso_Choche {
 	public static void main(String[] args) {
 
 		Coche Renault = new Coche(); // Instanciar la clase le damos el nombre al objeto y llamamos al constructor ()
-		
-		System.out.println("La cantidad de Ruedas que tiene es : " + Renault.getRuedas() + " ruedas");
-		System.out.println("El largo del coche es : " + Renault.getLargo() + " Cm");
 
+		// Renault.establece_color();
+		Renault.establece_color("Marron");
+		//Renault.color= "Rosa";
+		System.out.println(Renault.dime_datos_generales());
+		System.out.println(Renault.dime_color());
 	}
 }
